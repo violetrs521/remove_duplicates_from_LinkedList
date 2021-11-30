4034752f5294ef60cdbd3857f015dee183553c2b
Violet Smith <vsmith@Violets-MBP.attlocal.net>
2021-11-30 12:55:05 -0500

    cleaned up code

--- no_doubles_more_efficient.java
+++ no_doubles_more_efficient.java
@@ -3,16 +3,21 @@
 import java.util.*;
 public class no_doubles_more_efficient {
 
-    
-
+   
+    // removes the doubles from a linked list
     public static LinkedList<Integer> check(LinkedList<Integer> x){
+        //creates a hashmap to store the values of the linked list in 
         HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
         int count = x.size();
         for (int i = 0; i < count ; i++){
           
+            // if it is the first value of the linked list, add that value to the hashmap
             if(i == 0){
                 map.put(x.get(i), true);
             }
+
+            //if the map contains the value of the linked list at position i, it removes the value from the linked list
+            // it then decrements the count and the iterator since there is one less value in the linked list
             else if(map.containsKey(x.get(i))){
                 x.remove(i);
                 count = count - 1;
@@ -20,9 +25,8 @@ public class no_doubles_more_efficient {
                 
             }
             else {
-                
+                //adds the linked list value at position i to the hashmap
                 map.put(x.get(i), true);
-                
             }
 
         }
@@ -31,6 +35,7 @@ public class no_doubles_more_efficient {
         return x;
     }
 
+    //prints a linked list in brackets 
     public static void print(LinkedList<Integer> x){
         System.out.print("[");
         for(int i = 0; i<x.size(); i++){
@@ -39,10 +44,21 @@ public class no_doubles_more_efficient {
         System.out.println("]");
     }
 
+    //prints a hashmap (For testing)
     public static void printHash(HashMap<Integer, Integer> x){
-        
         System.out.println(x);
     }
+
+    // prints an iterator
+    public static void printIterator(ListIterator<Integer> it){
+        
+        while(it.hasNext()){
+            System.out.print(it.next());
+            System.out.print(" ");
+        }
+
+    }
+
 
     public static void main(String[] args) throws Exception {
 
@@ -52,12 +68,31 @@ public class no_doubles_more_efficient {
         one.add(2);
         one.add(4);
         one.add(4);
+
+        //prints the first list before the doubles are removed
+        System.out.println();
         System.out.println("This is the original first List: ");
         print(one);
         System.out.println(" ");
+
+        //prints and sets the first linked list iterator
+        ListIterator<Integer> oneIt = one.listIterator();
+        System.out.println("List one Iterator: ");
+        printIterator(oneIt);
+        System.out.println(" ");
+        System.out.println(" ");
         
+        //prints the first list after the doubles are removed
         System.out.println("This is the checked first list: ");
         print(check(one));
+        System.out.println(" ");
+        
+        //prints and sets the first linked list iterator after the doubles are removed
+        ListIterator<Integer> oneItChecked = one.listIterator();
+        System.out.println("List one Iterator Checked: ");
+        printIterator(oneItChecked);
+        System.out.println(" ");
+        System.out.println(" ");
         System.out.println(" ");
         System.out.println(" ");
 
@@ -70,12 +105,31 @@ public class no_doubles_more_efficient {
         two.add(4);
         two.add(4);
         two.add(5);
+    
+
+        //prints the second list before taking out the doubles
         System.out.println("This is the second original list: ");
         print(two);
         System.out.println(" ");
         
+        // Sets a Iterator for the second linked list and prints it
+        ListIterator<Integer> twoIt = two.listIterator();
+        System.out.println("List two Iterator: ");
+        printIterator(twoIt);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        //prints the second list after taking out the doubles
         System.out.println("This is second checked list: ");
         print(check(two));
+        System.out.println(" ");
+        
+        //prints the second linked list iterator after the doubles are taken out
+        ListIterator<Integer> twoItChecked = two.listIterator();
+        System.out.println("List two Iterator Checked: ");
+        printIterator(twoItChecked);
+        System.out.println(" ");
+        System.out.println(" ");
         System.out.println(" ");
         System.out.println(" ");
 
@@ -94,14 +148,74 @@ public class no_doubles_more_efficient {
         three.add(8);
         three.add(4);
       
+
+        //prints the third list before the doubles are taken out
         System.out.println("This is the third original List: ");
         print(three);
         System.out.println(" ");
+        
+        //prints and sets the third linked list iterator 
+        ListIterator<Integer> threeIt = three.listIterator();
+        System.out.println("List three Iterator: ");
+        printIterator(threeIt);
+        System.out.println(" ");
+        System.out.println(" ");
 
+        //prints the third list after the doubles are removed
         System.out.println("This is the third checked list");
         print(check(three));
+        System.out.println(" ");
+       
+      
+        //prints and sets the Iterator for the third list after the doubles are removed
+        ListIterator<Integer> threeItChecked = three.listIterator();
+        System.out.println("List three Iterator Checked: ");
+        printIterator(threeItChecked);
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
         
 
+     //Linked list test 4
+        LinkedList<Integer> four = new LinkedList<Integer>();
+        four.add(1);
+        four.add(2);
+        four.add(3);
+        four.add(4);
+        four.add(4);
+        four.add(5);
+        four.add(7);
+        four.add(10);
+        four.add(10);
+        four.add(10);
+        four.add(10);
+        four.add(10);
+        four.add(10);
+      
+        //prints the fourth linked list before the doubles are removed
+        System.out.println("This is the fourth original List: ");
+        print(four);
+        System.out.println(" ");
+
+        //prints and sets the fourth linked list iterator 
+        ListIterator<Integer> fourIt = four.listIterator();
+        System.out.println("List fourth Iterator: ");
+        printIterator(fourIt);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        //prints the fourth linked list after the doubles are taken out
+        System.out.println("This is the fourth checked list");
+        print(check(four));
+        System.out.println(" ");
+
+        //prints and sets the Iterator for the fourth list after the doubles are removed
+        ListIterator<Integer> fourItChecked = four.listIterator();
+        System.out.println("List fourth Iterator Checked: ");
+        printIterator(fourItChecked);
+        System.out.println(" ");
+        
 
 
     }
